aceaf6dd0924bf49ef5f15f9288e2e14490e70ea
Natalia Lira <natalia.rlira@gmail.com>
2020-05-09 06:28:02 -0300

    finalizado

--- js/script.js
+++ js/script.js
@@ -1,20 +1,29 @@
 let cep = document.getElementById('cep');
 let envioCep = document.getElementById('envioCep');
+let bairro = document.getElementById("bairro");
+let rua = document.getElementById("rua");
 
 envioCep.onclick = function (){
 
-        let config = {
+        if((cep.value).length == 8){
+            
+            let config = {
             headers:{
                 "Content-Type":"application/json"
             },
             method:"GET",
 
+            }
+            fetch('https://viacep.com.br/ws/'+cep.value+'/json/', config)
+            .then(function(resposta){
+            return resposta.json();
+            }).then(function(json){
+                rua.value = json.logradouro;
+                bairro.value = json.bairro;
+                console.log(json);
+            })
+        } else {
+            alert("Digite um cep valido")
         }
-        fetch('https://viacep.com.br/ws/03191160/json/', config)
-        .then(function(resposta){
-        return resposta.json();
-        }).then(function(json){
-            console.log(json);
-        })
 
 }
